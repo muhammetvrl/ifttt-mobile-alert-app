645be03f1bbf37cda7230e7d6a69d3e3ded664a0
Muhammet Varol <vrl.muhammet@gmail.com>
2019-04-14 16:30:01 +0300

    mongolab

--- helper/db_connect.js
+++ helper/db_connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
   const mongoose = require('mongoose');
-  mongoose.connect('mongodb://root:abc123@ds139896.mlab.com:39896/heroku_t5v8jh36', { useNewUrlParser: true });
+  mongoose.connect('mongodb://heroku_t5v8jh36:28bb607pp2deab5iqace8vf4bv@ds139896.mlab.com:39896/heroku_t5v8jh36', { useNewUrlParser: true });
   mongoose.connection.on('open', () => {
      console.log('MongoDB: Connected');
   });
